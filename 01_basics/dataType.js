eedbda965e3d4852d6323305cf4a2d9febd9c287
CodeChat-WithHanzla <hanzlaasam@gmai.com>
2024-03-05 23:36:34 +0500

    Data Type and some info of ECMASCRIPT

---
+++ 01_basics/dataType.js
@@ -0,0 +1,9 @@
+//"use strick";//Treat as newer version of JavaScript
+/*
+Data Types 
+Number,bigint,String,boolean,null,undefined,object,symbol
+*/
+console.log(typeof null)
+//so Type of null is object
+console.log(typeof undefined)
+//And Type of undefined is undefined
