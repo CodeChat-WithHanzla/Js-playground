3e447e84650f921f5940ec71f7e63608d0c54238
CodeChat-WithHanzla <hanzlaasam@gmai.com>
2024-03-05 21:20:55 +0500

    Variables in JavaScript

---
+++ 01_basics/variable.js
@@ -0,0 +1,26 @@
+/*There are three types of variables
+1-let
+2-const
+3-var
+Note: Using var isnt a good practice
+we can store any data type in above variables
+*/
+let name="Hanzla"
+name="Huzaifa" 
+//now let is overriden
+console.log(name)
+/*
+let name="khuzaima"
+console.log(name)
+this will give you error bcoz let name is declare
+block scope is the main reason using let */
+const num=293598;
+console.log(num)
+/*
+num=56229
+console.log(num)
+This will give you a error "assignment to const variable" */
+console.table([name,num])
+let age 
+console.log(age)
+//value is not defined so undefined will be the output
