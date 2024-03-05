edfb854e1e964669406ec33295559d919b39c3cc
CodeChat-WithHanzla <hanzlaasam@gmai.com>
2024-03-06 00:21:37 +0500

    Simple Data Type Conversion

---
+++ 01_basics/dataType-conversion.js
@@ -0,0 +1,11 @@
+let num="33xyz"
+let num1=Number(num)
+console.log(typeof num1)
+console.log(num1)
+let islogged=1
+let boollogged=Boolean(islogged)
+console.log(boollogged)
+let string=2024
+let string1=String(string)
+console.log(string1)
+console.log(typeof string1)
