2043254e6f8864003f2f84645ff1d84ffd1b728f
CodeChat-WithHanzla <162286057+CodeChat-WithHanzla@users.noreply.github.com>
2024-03-06 14:51:17 +0000

    Confusion about conversion between String & Number

---
+++ 01_basics/string-number.js
@@ -0,0 +1,18 @@
+let str1="Abu "
+let str2=" Hanzla"
+let str=str1+str2
+console.log(str)
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+3+4)
+console.log(1+3+"4")
+console.log(3+4*5%3)
+console.log(+true)//This give 1 as input
+//console.log(true+)This will give an error
+console.log(+"")
+let num1,num2,num3
+num1=num2=num3=2 + 2
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+//Note:Search prefix,postfix from MDN
