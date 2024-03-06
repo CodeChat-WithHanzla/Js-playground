b669dbddb1c7484b61346e71a59138995f50a506
CodeChat-WithHanzla <162286057+CodeChat-WithHanzla@users.noreply.github.com>
2024-03-06 18:45:40 +0000

    Stack Heap (memory managmenent)

---
+++ 01_basics/Stack-heap.js
@@ -0,0 +1,16 @@
+// Stack(Primitive),Heap(Non-Primitive)
+let name="Hanzla"
+let name1=name
+name1="Huzaifa"
+console.log(name);
+console.log(name1);
+//Heap
+const obj={
+    name:"Hanzla",
+    age:19,
+    email:"hanzlaasam@gmail.com"
+
+}
+ const obj1=obj
+ obj1.name="Huzaifa"
+ console.log(obj.name);
