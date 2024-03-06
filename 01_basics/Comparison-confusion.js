24469cc5549e237fe078b3c43a982d057715849e
CodeChat-WithHanzla <162286057+CodeChat-WithHanzla@users.noreply.github.com>
2024-03-06 15:27:30 +0000

    Confusions about Comparisons

---
+++ 01_basics/Comparison-confusion.js
@@ -0,0 +1,12 @@
+// Comparison >,<,>=,<=
+console.log("2">1);
+console.log("02">1);
+//NULL CONFUSION
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//UNDEFINED CONFUSION
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+// === compare both Value & Data Type
