290a3c2a27c025cad4c410917a5e8728642729b2
CodeChat-WithHanzla <hanzlaasam@gmai.com>
2024-03-13 19:28:36 +0500

    De-Structing and some info about JSON

---
+++ 02_basics/08_DeStructingOfObject.js
@@ -0,0 +1,25 @@
+const user={
+    name:"Hanzla",
+    age:19,
+    program:"BSCS",
+    semester:"2nd Semester"
+}
+//user.name
+const {name:nam}=user
+console.log(nam);//Now we can access name which is now nam directly
+
+
+// Json (JavaScript Object Notation)
+
+// JavaScript has a built in function for converting JSON strings into JavaScript objects:
+// JSON.parse()
+
+// JavaScript also has a built in function for converting an object into a JSON string:
+// JSON.stringify()
+
+
+
+
+
+
+
