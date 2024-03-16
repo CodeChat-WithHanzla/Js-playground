99b19d9d80ccfa4735a13b86114f37b6cc4199cf
CodeChat-WithHanzla <hanzlaasam@gmai.com>
2024-03-16 19:55:39 +0500

    Do and Do-While loop

--- 05_Iteration/02_whileDo-WhileLoop.js
+++ 05_Iteration/02_whileDo-WhileLoop.js
@@ -1,18 +1,19 @@
-
-/*let index=1
+let index=1
 while (index<=10) {
     console.log(`Value of Index is ${index}`);
     index++
 }
-*/
+
 let myArray=["Hanzla","Nameer","Ehsan Raza"]
 let arr=0
-/*while (arr<myArray.length) {
+while (arr<myArray.length) {
     console.log(`Value at ${arr} is ${myArray[arr]}`);
     arr=arr+1
-}*/
+}
 
 do {
+    //First execute the blockn of code then check the condition
     console.log(`Value at ${arr} is ${myArray[arr]}`);
     arr=arr+1 
 } while (arr<0);
+
